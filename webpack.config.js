36d0e555699a865a099ca38d178224958ee03774
Method Education school <90511114+maksim-leskin@users.noreply.github.com>
2022-06-23 03:05:36 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
     filename: '[name].[contenthash].js',
-    assetModuleFilename: 'assets/[hash][ext][query]]',
+    assetModuleFilename: 'assets/[hash][ext][query]',
   },
   plugins: [
     new HtmlWebpackPlugin({
