a715ee53281898197f09021a637c3e8a3ac28fc9
Maksim Leskin <maksim.leskin@skillbox.ru>
2022-12-21 01:17:49 +0300

    add plugin copy static

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const CopyPlugin = require('copy-webpack-plugin');
 
 const mode = process.env.NODE_ENV || 'development';
 const devMode = mode === 'development';
@@ -30,6 +31,9 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
     }),
+    // new CopyPlugin({
+    //   patterns: [{ from: 'static', to: './' }],
+    // }),
   ],
   module: {
     rules: [
