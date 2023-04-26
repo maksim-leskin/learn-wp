3fe46642ce0f8ac236fc02f52c53eeeb1382fd64
Maksim Leskin <maksim.leskin@skillbox.ru>
2023-04-26 19:11:16 +0300

    update ver TS 5

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,31 @@
 import './index.html';
 import './index.scss';
-import code from './img/code.png'
-import {
-  mult,
-  sum
-} from './modules/calc';
+import url from './img/code.png';
+import { mult, sum } from './modules/calc';
 
+const createImage = (url: string):HTMLImageElement => {
+  const img = new Image();
+  img.src = url;
+  img.width = 700;
+  return img;
+};
 
-const imgWrap = document.querySelector('.img');
-const img = new Image();
-img.src = code;
-img.width = 700;
+const renderImage = () => {
+  const imgWrap = document.querySelector('.img');
+  const image = createImage(url);
 
-if (imgWrap instanceof HTMLDivElement) {
-  imgWrap.append(img);
-}
+  if (imgWrap instanceof HTMLDivElement) {
+    imgWrap.append(image);
+  }
+};
 
+const init = async () => {
+  const x: number = 5;
+  const y: number = 21;
 
-console.log(mult(3, 4));
-console.log(sum(3, 4));
+  console.log('mult(x, y): ', mult(x, y));
+  console.log('sum(y, x): ', await sum(y, x));
+  renderImage();
+};
+
+init();
